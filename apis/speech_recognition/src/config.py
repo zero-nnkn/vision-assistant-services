f2ab3ce9989f4db5f0cdd836c6b6591a2afcb7d3
Zero-nnkn <nnkhoinguyen9@gmail.com>
2023-08-15 15:35:40 +0700

    🔧 Add model info to config

--- apis/speech_recognition/src/config.py
+++ apis/speech_recognition/src/config.py
@@ -3,15 +3,22 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings
 
+
 FILE = Path(__file__)
 ROOT = str(FILE.parent.parent)
 
 
 class Settings(BaseSettings):
+    # PROJECT INFORMATION 
     HOST: str
     PORT: int
     CORS_ORIGINS: list[str]
     CORS_HEADERS: list[str]
+
+    # ML INFORMATION
+    FASTER_WHISPER_MODEL: str
+    FASTER_WHISPER_MODEL_DEVICE: str
+    FASTER_WHISPER_MODEL_COMPUTE_TYPE: str
 
     class Config:
         env_file = os.path.join(ROOT, '.env')
