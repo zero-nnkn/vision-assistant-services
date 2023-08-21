3b32bd8f956d5a0b2b8bc3c2a1aa9a7154eca96b
ngxquang <nq23092016@gmail.com>
2023-08-21 10:48:07 +0700

    perf: download model and save to image before starting up

--- apis/TTS/src/text2speech/service.py
+++ apis/TTS/src/text2speech/service.py
@@ -9,15 +9,50 @@ class Transcriber:
     Transcriber class to transcribe audio file to text using pretrained Faster Whisper.
     """
 
-    def __init__(self, model_name: str = "tts_models/en/ljspeech/vits", device: str = "cpu"):
-        self.load_model(model_name=model_name, device=device)
+    def __init__(
+        self,
+        model_name: str = None,
+        model_path: str = None,
+        config_path: str = None,
+        vocoder_path: str = None,
+        vocoder_config: str = None,
+        device: str = "cpu",
+    ):
+        if model_name is not None:
+            self.load_model_by_name(model_name=model_name, device=device)
+        elif model_path is not None:
+            self.load_model_by_path(
+                model_path, config_path, vocoder_path, vocoder_config, device=device
+            )
 
     @lru_cache(maxsize=1)
-    def load_model(self, model_name: str = "tts_models/en/ljspeech/vits", device: str = "cpu"):
+    def load_model_by_name(
+        self, model_path: str = "tts_models/en/ljspeech/vits", device: str = "cpu"
+    ):
         if device == "gpu" and not torch.cuda.is_available():
             print("No CUDA device is available")
             device = "cpu"
-        self.model = TTS(model_name, gpu=True if device == "gpu" else False)
+        self.model = TTS(model_path=model_path, gpu=True if device == "gpu" else False)
+
+    @lru_cache(maxsize=1)
+    def load_model_by_path(
+        self,
+        model_path: str,
+        config_path: str,
+        vocoder_path: str,
+        vocoder_config: str,
+        device: str = "cpu",
+    ):
+        if device == "gpu" and not torch.cuda.is_available():
+            print("No CUDA device is available")
+            device = "cpu"
+        self.model = TTS(
+            model_path=model_path,
+            config_path=config_path,
+            vocoder_path=vocoder_path,
+            vocoder_config_path=vocoder_config,
+            gpu=True if device == "gpu" else False,
+        )
 
     def transcribe(
         self,
